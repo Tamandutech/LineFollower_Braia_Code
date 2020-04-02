5c9f0d7144c0901470769e7a2261477f6f1a2acf
Raphera <raphaeldefreitas2009@gmail.com>
2020-04-01 22:27:54 -0300

    Biblioteca ESP32Encoder portada para framework IDF

---
+++ ESP-IDF/main/io.h
@@ -0,0 +1,24 @@
+
+// Sensores laterais
+#define SL1 39
+#define SL2 36
+#define SL3 32
+#define SL4 33
+
+// Motores
+#define MOT_ESQ_F 27
+#define MOT_ESQ_R 13
+#define MOT_DIR_F 25
+#define MOT_DIR_R 26
+
+// Encoders Motores
+#define ENC_MOT_ESQ_A 34
+#define ENC_MOT_ESQ_B 35
+#define ENC_MOT_DIR_A 04
+#define ENC_MOT_DIR_B 16
+
+// SPI do ADC (Utiliza VSPI do ESP32)
+#define ADC_CLK 18
+#define ADC_DOUT 19
+#define ADC_DIN 23
+#define ADC_CS 22
--- ESP-IDF/main/main.cpp
+++ ESP-IDF/main/main.cpp
@@ -1,5 +1,7 @@
+#include "ESP32Encoder.h"
 #include "QTRSensors.h"
 #include "esp_log.h"
+#include "io.h"
 
 extern "C" {
 void app_main(void);
@@ -7,14 +9,23 @@ void app_main(void);
 
 QTRSensors qtr;
 
+// Instancia dos encodes
+ESP32Encoder enc_esq;
+ESP32Encoder enc_dir;
+
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
 
 void app_main(void) {
   // Inicializa o Driver do MCP3008
   qtr.setTypeMCP3008();
-  qtr.setSensorPins((const uint8_t[]){0, 1, 2, 3, 4, 5, 6, 7}, SensorCount, GPIO_NUM_19,
-                    GPIO_NUM_23, GPIO_NUM_18, GPIO_NUM_22, 1350000, VSPI_HOST);
+  qtr.setSensorPins((const uint8_t[]){0, 1, 2, 3, 4, 5, 6, 7}, SensorCount,
+                    GPIO_NUM_19, GPIO_NUM_23, GPIO_NUM_18, GPIO_NUM_22, 1350000,
+                    VSPI_HOST);
+
+  // Anexa os IOs a instancia do contador dos encoders
+  enc_dir.attachHalfQuad(ENC_MOT_DIR_A, ENC_MOT_DIR_B);
+  enc_esq.attachHalfQuad(ENC_MOT_ESQ_A, ENC_MOT_ESQ_B);
 
   gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT_OUTPUT);
   gpio_set_level(GPIO_NUM_2, 1);
@@ -41,26 +52,14 @@ void app_main(void) {
 
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-  int64_t time = 0;
-
   while (1) {
-    // read calibrated sensor values and obtain a measure of the line position
-    // from 0 to 5000 (for a white line, use readLineWhite() instead)
-    int16_t position = qtr.readLineWhite(sensorValues);
 
-    // print the sensor values as numbers from 0 to 1000, where 0 means maximum
-    // reflectance and 1000 means minimum reflectance, followed by the line
-    // position
-    time = esp_timer_get_time();
-
-    for (uint8_t i = 0; i < SensorCount; i++) {
-      printf("%d", sensorValues[i]);
-      printf("\t");
-    }
-    
-    printf("%d", position);
+    printf("%d", enc_dir.getCount());
+    printf("\t");
+    printf("%d", enc_esq.getCount());
+    printf("\t");
     printf("\n");
-    
+
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
