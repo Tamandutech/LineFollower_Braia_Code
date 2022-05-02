ab4b3d36125c526e577ddb7d9f37563f18497f42
Raphael Henrique Nunes de Freitas <50784405+raphera@users.noreply.github.com>
2022-05-01 21:40:17 -0300

    Variável renomeada.

--- modules/robot/services/PIDService/src/PIDService.hpp
+++ modules/robot/services/PIDService/src/PIDService.hpp
@@ -39,7 +39,7 @@ private:
     // Variáveis para cálculo do pid rot e trans
     float rotK = 5;
     float accel = 6000; // aceleração em rpm/s
-    int16_t setpointTarget = 0;
+    int16_t setpointPIDTransTarget = 0;
     int16_t newSetpoint = 0;
     float PidTrans = 0;
     float Ptrans = 0, Itrans = 0, Dtrans = 0;
