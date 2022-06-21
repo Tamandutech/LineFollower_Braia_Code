89565eea914a3945a8634ccc6be0a9b16f1377bf
Raphael Henrique Nunes de Freitas <50784405+raphera@users.noreply.github.com>
2022-06-21 04:13:50 +0000

    Fix QEMU do robô. 👌

--- modules/gateway/components/WiFiHandler/src/WiFiHandler.cpp
+++ modules/gateway/components/WiFiHandler/src/WiFiHandler.cpp
@@ -67,6 +67,9 @@ void WiFiHandler::wifi_init_sta(void)
     {
         ESP_ERROR_CHECK(esp_netif_init());
         ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+#ifndef ESP32_QEMU
+
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
         ESP_ERROR_CHECK(esp_wifi_init(&cfg));
         ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
@@ -74,6 +77,10 @@ void WiFiHandler::wifi_init_sta(void)
         ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
         ESP_ERROR_CHECK(esp_wifi_start());
         ESP_ERROR_CHECK(esp_wifi_set_channel(EXAMPLE_ESP_WIFI_CHANNEL, WIFI_SECOND_CHAN_NONE));
+
+#else
+        example_connect();
+#endif
         wifiAlreadyInit = true;
     }
     else
