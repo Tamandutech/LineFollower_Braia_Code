2b173c419d833fdd9f0f3318a113683182115d01
Matheus <matheus.sosua3089@gmail.com>
2024-02-16 16:30:19 -0300

    refactor: remoção de arquivos desnecessários

--- modules/common/components/CMDWrapper/cmd_wifi/README.md
+++
@@ -1,2 +0,0 @@
-# cmd_wifi
-Esta pasta contém os comandos de texto relacionados com as informações da conexão wifi.
--- modules/common/components/CMDWrapper/cmd_wifi/cmd_wifi.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-void register_cmd_wifi(void);
-
-static std::string channel_get(int argc, char **argv);
-
-void register_channel_get(void);
