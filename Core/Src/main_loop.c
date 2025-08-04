1466faa3b2ee8e0263d33f14dc59a9cbf48bc361
Fabrízio <51520615+FabrizioRP@users.noreply.github.com>
2025-08-04 10:37:07 -0300

    Typesetting is my passion

--- Core/Src/main_loop.c
+++ Core/Src/main_loop.c
@@ -201,7 +201,8 @@ void motorControl(float desiredVoltage) {
         if (rightSpeed > MAX_SPEED) rightSpeed = MAX_SPEED;
         write_pin(motorDirDir, 1);
 
-    } else {
+    } 
+    else {
         if (rightSpeed < -MAX_SPEED) rightSpeed = -MAX_SPEED;
         rightSpeed = (-1) * rightSpeed;
         write_pin(motorDirDir, 0);
@@ -211,7 +212,8 @@ void motorControl(float desiredVoltage) {
         if (leftSpeed > MAX_SPEED) leftSpeed = MAX_SPEED;
         write_pin(motorEsqDir, 1);
 
-    } else {
+    } 
+    else {
         if (leftSpeed < -MAX_SPEED) leftSpeed = -MAX_SPEED;
         leftSpeed = (-1) * leftSpeed;
         write_pin(motorEsqDir, 0);
@@ -281,9 +283,11 @@ void controla_suc(uint16_t target) {
         suc_ok = 0;
         if (target > last_pwm) {
             last_pwm++;
-        } else if (target < last_pwm) {
+        } 
+        else if (target < last_pwm) {
             last_pwm--;
-        } else {
+        } 
+        else {
             suc_ok = 1;
         }
 
@@ -344,9 +348,11 @@ void main_loop(void) {
 
                 if (encoder_values[1] < 400) {
                     motorControl(0.7f);
-                } else if (encoder_values[1] < 700) {
+                } 
+                else if (encoder_values[1] < 700) {
                     motorControl(0.6f);
-                } else if (encoder_values[1] < 5500) {
+                } 
+                else if (encoder_values[1] < 5500) {
                     motorControl(0.7f);
                 }
 
