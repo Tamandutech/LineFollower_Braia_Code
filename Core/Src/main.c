51566b47f067498ac0f27562398d17f8877ac6a6
Kelvin Novais <kelvin.novais@protonmail.com>
2025-10-04 17:07:09 -0300

    Attempt to solve BOOT buttom problem
    This commit changes the .ioc
    It tries to solve the need to hold the BOOT button to start the robot. Unsuccessful.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -895,7 +895,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
@@ -993,7 +993,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : Boot_sw_Pin */
   GPIO_InitStruct.Pin = Boot_sw_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(Boot_sw_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : IMU_INT_Pin */
