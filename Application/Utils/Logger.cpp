88a993f1951095b7070139236f0ebe48cfbf7d3f
Kelvin Novais <kelvin.novais@protonmail.com>
2025-10-31 12:20:14 -0300

    Add Logger

---
+++ Application/Utils/Logger.cpp
@@ -0,0 +1,146 @@
+/*
+ * Logger.cpp
+ *
+ *  Created on: Oct 25, 2025
+ *      Author: Kelvin Novais
+ */
+
+#include "Logger.hpp"
+
+#include "stm32g4xx_hal.h"
+
+#include <cstdio>
+#include <cstring>
+
+// Initialize class variables
+char Logger::buffer[LOGGER_BUFFER_SIZE] = {0};
+char Logger::formatted[LOGGER_FORMATTED_BUFFER] = {0};
+bool Logger::useDoubleBreak = false;
+bool Logger::showLogLevel = true;
+
+extern "C" {
+  extern UART_HandleTypeDef huart1;
+}
+
+Logger::Logger(const char *newTag,
+               bool newShowTag,
+               Level newLevel) : tag(nullptr) {
+  level = Error | Warning | newLevel;
+  showTag = newShowTag;
+
+  if (newTag) {
+    // Allocate memory for the tag
+    // (+3 for the space, colon, and null terminator)
+    const int size = strlen(newTag) + 3;
+    tag = new char[size];
+
+    // Format the incoming string to the instance tag
+    snprintf(tag, size, "%s: ", newTag);
+  } else {
+    const char *noTag = "";
+
+    tag = new char[strlen(noTag)];
+    strcpy(tag, noTag);
+  }
+}
+
+Logger::~Logger() {
+  delete[] tag;
+}
+
+void Logger::formatLog(const char *logLevelStamp) {
+  // [logLevelStamp] tag: buffer useDoubleBreak
+  snprintf(formatted, LOGGER_FORMATTED_BUFFER,
+           "%s%s%s%s",
+           (showLogLevel && logLevelStamp) ? logLevelStamp : "",
+           (showTag) ? tag : "",
+           buffer,
+           (useDoubleBreak) ? "\n\n" : "\n");
+}
+
+void Logger::sendLog() {
+	// TODO treat the return of the function to avoid messing the messages
+  HAL_UART_Transmit_DMA(&huart1,
+                        (const uint8_t *) formatted,
+                        strlen(formatted));
+}
+
+void Logger::error(const char *format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  vsnprintf(buffer, LOGGER_BUFFER_SIZE, format, args);
+	formatLog("[E] ");
+
+  sendLog();
+
+  va_end(args);
+}
+
+void Logger::warning(const char *format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  vsnprintf(buffer, LOGGER_BUFFER_SIZE, format, args);
+	formatLog("[W] ");
+
+  sendLog();
+
+  va_end(args);
+}
+
+void Logger::info(const char *format, ...) {
+  va_list args;
+
+  if ((level & Info) == 0)
+    return;
+
+  va_start(args, format);
+  vsnprintf(buffer, LOGGER_BUFFER_SIZE, format, args);
+	formatLog("[I] ");
+
+  sendLog();
+
+  va_end(args);
+}
+
+void Logger::debug(const char *format, ...) {
+  va_list args;
+
+  if ((level & Debug) == 0)
+    return;
+
+  va_start(args, format);
+  vsnprintf(buffer, LOGGER_BUFFER_SIZE, format, args);
+	formatLog("[D] ");
+
+  sendLog();
+
+  va_end(args);
+}
+
+void Logger::setUseDoubleBreak(bool useDoubleBreak) {
+  Logger::useDoubleBreak = useDoubleBreak;
+}
+
+void Logger::setShowLogLevel(bool showLogLevel) {
+  Logger::showLogLevel = showLogLevel;
+}
+
+void Logger::log(const char* format, ...) {
+  va_list args;
+
+  va_start(args, format);
+
+  vsnprintf(buffer, LOGGER_BUFFER_SIZE, format, args);
+
+  snprintf(formatted, LOGGER_FORMATTED_BUFFER,
+           "%s%s",                              // format
+           buffer,                              // the message
+           (useDoubleBreak) ? "\n\n" : "\n");   // double break or not
+
+
+  sendLog();
+
+  va_end(args);
+}
---
+++ Application/Utils/Logger.hpp
@@ -0,0 +1,64 @@
+/*
+ * Logger.hpp
+ *
+ *  Created on: Oct 25, 2025
+ *      Author: Kelvin Novais
+ */
+
+#ifndef UTILS_LOGGER_HPP_
+#define UTILS_LOGGER_HPP_
+
+#include <cstdarg>
+#include <cstdint>
+
+#define LOGGER_BUFFER_SIZE ((unsigned int) 512)
+#define LOGGER_FORMATTED_BUFFER (LOGGER_BUFFER_SIZE + 32)
+
+class Logger {
+public:
+  // Defining a type for the log level
+  enum Level : uint8_t {
+    None    = 0,
+    Error   = 1 << 0,
+    Warning = 1 << 1,
+    Info    = 1 << 2,
+    Debug   = 1 << 3
+  };
+
+  // Constructor and destructor
+  Logger(const char *tag, bool showTag, Level flags);
+  ~Logger();
+
+  // Class wide methods
+  static void setUseDoubleBreak(bool useDoubleBreak);
+  static void setShowLogLevel(bool showLogLevel);
+  static void log(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+  // Per instance methods
+  void error(const char *format, ...) __attribute__((format(printf, 2, 3)));
+  void warning(const char *format, ...) __attribute__((format(printf, 2, 3)));
+  void info(const char *format, ...) __attribute__((format(printf, 2, 3)));
+  void debug(const char *format, ...) __attribute__((format(printf, 2, 3)));
+
+private:
+  // Class wide variables and methods
+  static char buffer[LOGGER_BUFFER_SIZE];
+  static char formatted[LOGGER_FORMATTED_BUFFER];
+  static bool useDoubleBreak;
+  static bool showLogLevel;
+  
+  static void sendLog();
+  
+  // Per instance variables and methods
+  int level;
+  bool showTag;
+  char *tag;
+
+  void formatLog(const char *logLevelStamp);
+};
+
+#endif /* UTILS_LOGGER_HPP_ */
+
+/*
+  Maurice Ravel, Bolero
+*/
