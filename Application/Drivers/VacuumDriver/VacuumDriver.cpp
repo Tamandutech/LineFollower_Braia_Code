67b384fe6351ecd79ed4d72d6af4eee08223a0a7
Kelvin Novais <kelvin.novais@protonmail.com>
2025-10-31 18:49:20 -0300

    Add Vacuum

---
+++ Application/Drivers/VacuumDriver/VacuumDriver.cpp
@@ -0,0 +1,52 @@
+/*
+ * VacuumDriver.cpp
+ *
+ *  Created on: Oct 28, 2025
+ *      Author: Kelvin Novais
+ */
+
+#include "VacuumDriver.hpp"
+#include "../../Utils/Timer.hpp"
+#include "tim.h"
+
+#include <algorithm>
+
+#define MIN_VALUE ((uint16_t) 20)
+#define MAX_VALUE ((uint16_t) 1000)
+
+#define DELAY 2
+
+VacuumDriver::Pin VacuumDriver::pin = {
+  &htim5,  
+  TIM_CHANNEL_2
+};
+
+uint16_t VacuumDriver::lastPWM = 0;
+
+void VacuumDriver::pwmOutput(uint16_t target) {
+  target = std::max(target, MAX_VALUE);
+  target = std::min(target, MIN_VALUE);
+
+  __HAL_TIM_SET_COMPARE(pin.pwmhtim, pin.pwmChannel, target);
+
+  lastPWM = target;
+}
+
+void VacuumDriver::pwmAcceleratedOutput(uint16_t target) {
+  uint16_t currentValue = lastPWM;
+  
+  target = std::max(target, MAX_VALUE);
+  target = std::min(target, MIN_VALUE);
+
+  while (currentValue != target) {
+    if (currentValue < target) {
+      __HAL_TIM_SET_COMPARE(pin.pwmhtim, pin.pwmChannel, ++currentValue);
+      Timer::delayMiliseconds(DELAY);
+    } else if (currentValue > target) {
+      __HAL_TIM_SET_COMPARE(pin.pwmhtim, pin.pwmChannel, --currentValue);
+      Timer::delayMiliseconds(DELAY);
+    }
+  }
+  
+  lastPWM = target;
+}
---
+++ Application/Drivers/VacuumDriver/VacuumDriver.hpp
@@ -0,0 +1,32 @@
+/*
+ * VacuumDriver.hpp
+ *
+ *  Created on: Oct 28, 2025
+ *      Author: Kelvin Novais
+ */
+
+#ifndef DRIVERS_VACUUMDRIVER_VACUUMDRIVER_HPP_
+#define DRIVERS_VACUUMDRIVER_VACUUMDRIVER_HPP_
+
+#include "stm32g4xx_hal.h"
+
+#include <cstdint>
+
+class VacuumDriver {
+public:
+  static void pwmOutput(uint16_t duty);
+  static void pwmAcceleratedOutput(uint16_t duty);
+
+private:
+  typedef struct _Pin {
+    // Motor PWM pin
+    TIM_HandleTypeDef *pwmhtim;
+    // PWM channel
+    uint32_t pwmChannel;
+  } Pin;
+
+  static Pin pin;
+  static uint16_t lastPWM;
+};
+
+#endif /* DRIVERS_VACUUMDRIVER_VACUUMDRIVER_HPP_ */
