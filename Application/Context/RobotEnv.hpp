fa2b5b0b3abb7bacb2e052a91f700a6988a38f5b
Kelvin Novais <kelvin.novais@protonmail.com>
2025-11-12 15:41:14 -0300

    Add RobotEnv

---
+++ Application/Context/RobotEnv.hpp
@@ -0,0 +1,40 @@
+/*
+ * RobotEnv.hpp
+ *
+ *  Created on: Nov 8, 2025
+ *      Author: Kelvin Novais
+ */
+
+#ifndef CONTEXT_ROBOTENV_HPP_
+#define CONTEXT_ROBOTENV_HPP_
+
+#include <cstdint>
+
+namespace RobotEnv {
+// TODO set unit (prefer SI [m])
+const int32_t ROBOT_WIDTH         = 4;
+const int32_t WHEEL_RADIUS        = 11;
+const int32_t WHEEL_CIRCUMFERENCE = 70;
+
+// const int32_t BASE_MOTOR_PWM  = 10;
+const int32_t BASE_VACUUM_PWM = 100;
+
+const int16_t MAX_MOTOR_PWM = 1000;
+
+// Tolerance for floating point comparisons
+constexpr float EPSILON_TOLERANCE = 1e-6F;
+
+// Constants for preventing integral windup in PID
+constexpr float INTEGRAL_MAX = 1000.0F;  // Maximum value for the full term
+constexpr float INTEGRAL_MIN = -1000.0F; // Minimum value for the full term
+
+namespace MotorDriver {
+// TODO set unit (prefer SI [m/s])
+const float   MAX_SPEED          = 1000; // TODO this is a PWM value by now
+const int32_t MAX_DECELERATION   = 4;
+const int32_t MAX_ROTATION_SPEED = 4;
+} // namespace MotorDriver
+
+} // namespace RobotEnv
+
+#endif /* CONTEXT_ROBOTENV_HPP_ */
