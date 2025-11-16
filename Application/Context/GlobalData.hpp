4862c5bb8ae33909881361bc3ae7b8106b733fde
Kelvin Novais <kelvin.novais@protonmail.com>
2025-11-16 20:43:08 -0300

    Add GlobalData

---
+++ Application/Context/GlobalData.hpp
@@ -0,0 +1,32 @@
+/*
+ * GlobalData.hpp
+ *
+ *  Created on: Nov 15, 2025
+ *      Author: Kelvin Novais
+ */
+
+#ifndef CONTEXT_GLOBALDATA_HPP_
+#define CONTEXT_GLOBALDATA_HPP_
+
+#include <atomic>
+#include <cstdint>
+#include <vector>
+
+typedef struct _MapPoint {
+  int32_t encoderAverage;
+  float   baseMotorPWM;
+} MapPoint;
+
+
+typedef struct _GlobalData {
+  std::atomic<bool> isReadyToRun;
+  std::atomic<int32_t> finishLineCount;
+  std::vector<MapPoint> mapData;
+  std::atomic<int32_t> markCount;
+} GlobalData;
+
+extern GlobalData globalData;
+
+#endif /* CONTEXT_GLOBALDATA_HPP_ */
+
+// Aram Khachaturian: Masquerade Suite
