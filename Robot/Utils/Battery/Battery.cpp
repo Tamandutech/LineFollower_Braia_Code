79a2f657109bb10152edcea89f3cf2a527dad062
Kelvin Novais <kelvin.novais@protonmail.com>
2026-05-25 15:21:09 -0300

    Compute average for battery voltage

--- Robot/Utils/Battery/Battery.cpp
+++ Robot/Utils/Battery/Battery.cpp
@@ -5,7 +5,7 @@
  *      Author: Kelvin Novais
  */
 
-#include "../../Utils/Battery/Battery.hpp"
+#include "Battery.hpp"
 
 #include "adc.h"
 
@@ -18,28 +18,18 @@ const volatile uint32_t *const Battery::rawReferenceVoltage = &adc1_buffer[8];
 
 float Battery::getBatteryVoltage() {
   // Calculates battery voltage based on ADC reading
+  const uint8_t samples = 255;
+  float result = 0;
 
-  // Vref = 1.2V, 4095 is the maximum value of ADC
-  float vref = 1.21F * 4095.0F / *rawReferenceVoltage;
+  for (uint8_t i = 0; i < samples; i++) {
+    // Vref = 1.2V, 4095 is the maximum value of ADC
+    float vref = 1.21F * 4095.0F / *rawReferenceVoltage;
+  
+    // 5.6875 is the voltage divider constant
+    float vbat = (*rawBatteryVoltage * vref / 4095.0F) * 5.6875F;
 
-  /*
-    TODO fix constant value
-    
-    BATTERY 1:
-      REAL      7.91 V
-      MESUARING 8.38 V
+    result += vbat;
+  }
 
-    BATTERY 2:
-      REAL      8.33 V
-      MEASURING 8.82 V
-
-    BATTERY 3:
-      REAL      8.09 V
-      MEASURING 8.57 V
-  */
-
-  // 5.6875 is the voltage divider constant
-  float vbat = (*rawBatteryVoltage * vref / 4095.0F) * 5.6875F;
-
-  return vbat;
+  return (result / samples);
 }
