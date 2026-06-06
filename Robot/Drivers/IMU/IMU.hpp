b56fe7ea8b6c8928b5dd2ab7ac7138e72de94731
Kelvin Novais <kelvin.novais@protonmail.com>
2026-06-06 08:22:43 -0300

    Add IMU functions
    - Still needs improvements
    - calibrate() doesn't work well

--- Robot/Drivers/IMU/IMU.hpp
+++ Robot/Drivers/IMU/IMU.hpp
@@ -15,36 +15,57 @@ class IMU {
 public:
   // m/s²
   static const float (&acceleration)[N_AXES_];
+  // m/s
+  static const float (&speed)[N_AXES_];
+  // m
+  static const float (&position)[N_AXES_];
 
   // °/s
-  static const float (&angularRate)[N_AXES_];
+  static const float (&angularRate)[N_ROTATION_AXES_];
+  // °
+  static const float (&angle)[N_ROTATION_AXES_];
 
   // K
   static const float &temperature;
 
   static void initialize();
-  static void update();
+  static void calibrate();
+  static void reset();
+  static void update(const uint32_t us_dt);
 
 private:
   // IMU related
-  static uint8_t      whoAmI;
-  static uint8_t      reset;
-  static stmdev_ctx_t context;
+  static uint8_t      whoAmI_;
+  static stmdev_ctx_t context_;
 
   // Raw data
-  static int16_t rawAcceleration[N_AXES_];
-  static int16_t rawAngularRate[N_ROTATION_AXES_];
-  static int16_t rawTemperature;
+  static int16_t rawAcceleration_[N_AXES_];
+  static int16_t rawAccelerationNoise_[N_AXES_];
+  static int16_t rawAngularRate_[N_ROTATION_AXES_];
+  static int16_t rawAngularRateNoise_[N_ROTATION_AXES_];
+  static int16_t rawTemperature_;
 
   // Treated data
+  static float previousAcceleration_[N_AXES_];
   static float acceleration_[N_AXES_];
-  static float angularRate_[N_ROTATION_AXES_]; // NOLINT
-  static float temperature_;                   // NOLINT
+  static float previousSpeed_[N_AXES_];
+  static float speed_[N_AXES_];
+  static float position_[N_AXES_];
 
-  static int32_t write(void *handle, uint8_t reg, const uint8_t *bufp,
-                       uint16_t len);
-  static int32_t read(void *handle, uint8_t reg, uint8_t *bufp, uint16_t len);
+  static float previousAngularRate_[N_ROTATION_AXES_];
+  static float angularRate_[N_ROTATION_AXES_];
+  static float angle_[N_ROTATION_AXES_];
+
+  static float temperature_;
+
+  static int32_t writeRegister(void *handle, uint8_t reg, const uint8_t *bufp,
+                               uint16_t len);
+  static int32_t readRegister(void *handle, uint8_t reg, uint8_t *bufp,
+                              uint16_t len);
   static void    delay(uint32_t ms);
+  static void    readRawValues();
 };
 
 #endif /* DRIVERS_IMU_IMU_HPP_ */
+
+// Telemann - Viola Concerto in G major, TWV 51:G9
