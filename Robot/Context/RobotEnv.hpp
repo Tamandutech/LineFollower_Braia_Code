03d8f298ee071b22bcbef0e06983461454b3b6bd
Kelvin Novais <kelvin.novais@protonmail.com>
2026-06-08 19:49:25 -0300

    Fix constants, add new ones, remove unused

--- Robot/Context/RobotEnv.hpp
+++ Robot/Context/RobotEnv.hpp
@@ -16,48 +16,58 @@
  * in order to avoid it being copied on every file this header is included.
  */
 namespace RobotEnv {
-const int32_t  ROBOT_WIDTH           = 4;
-const double   WHEEL_DIAMETER        = 0.0222;                // m
-const uint16_t PULSES_PER_REVOLUTION = 1024;                  // pulses/rev
-const double   WHEEL_RADIUS          = WHEEL_DIAMETER / 2.0;  // m
-const double   WHEEL_CIRCUMFERENCE   = M_PI * WHEEL_DIAMETER; // m
-const double   MM_PER_PULSE = WHEEL_CIRCUMFERENCE / PULSES_PER_REVOLUTION; // mm
-const float    GRAVITY      = 9.80665;            // m/s²
-const float    MILLI_GRAVITY = GRAVITY / 1000.0F; // m/s²
+const int32_t ROBOT_WIDTH = 4;
 
-// Tolerance for floating point comparisons
-constexpr float EPSILON_TOLERANCE = 1e-6F;
 
-// Constants for preventing integral windup in PID
-const float INTEGRAL_MAX = 1000.0F;  // Maximum value for the full term
-const float INTEGRAL_MIN = -1000.0F; // Minimum value for the full term
+const uint16_t PULSES_PER_REVOLUTION = 4095; // pulses/rev
+
+
+const float WHEEL_DIAMETER      = 0.0222F;               // m
+const float WHEEL_RADIUS        = WHEEL_DIAMETER / 2.0F; // m
+const float WHEEL_CIRCUMFERENCE = M_PI * WHEEL_DIAMETER; // m
+
+
+const float MM_PER_PULSE =
+    WHEEL_CIRCUMFERENCE / (PULSES_PER_REVOLUTION * 1000); // mm
+
+
+const uint8_t  TRACK_MAX_LENGTH = 60;                               // m
+const uint32_t TRACK_MAX_PULSES =
+    TRACK_MAX_LENGTH * PULSES_PER_REVOLUTION / WHEEL_CIRCUMFERENCE; // pulses
+const float  TRACK_MAP_DISTANCE = 0.02F;                            // m
+const size_t TRACK_MAP_N_POINTS = TRACK_MAX_LENGTH / TRACK_MAP_DISTANCE;
+
+
+const float GRAVITY       = 9.80665;           // m/s²
+const float MILLI_GRAVITY = GRAVITY / 1000.0F; // m/s²
+
 
 const float kp = 0.1;
 const float kd = 1.9;
 
-// struct PIDParameter {
-//   float base;
-//   float translational;
-// };
 
-// const PIDParameter Kp = {.base = 0.100, .translational = 01.000}; // NOLINT
-// const PIDParameter Kd = {.base = 1.900, .translational = 20.000}; // NOLINT
-// const PIDParameter Ki = {.base = 0.000, .translational = 04.600}; // NOLINT
+const uint16_t BASE_LOOP_TIME_US = 1000;  // µs
+const uint16_t MAX_OUT_TIME_US   = 60000; // µs
+// The min time to consider valid a second reading of the right mark
+const uint32_t MIN_TRACK_TIME = 2 * 1e6; // µs
 
-const uint16_t BASE_LOOP_TIME_US = 1000;                  // µs
 
-const int32_t MOTOR_BASE_PWM           = 100;             // ‰
-const float   MOTOR_BASE_SPEED         = 0.75F;           // m/s
-const int16_t MOTOR_MAX_PWM            = 1000;            // ‰
-const float   MOTOR_MAX_SPEED          = 4;               // m/s
-const int32_t MOTOR_MAX_DECELERATION   = 4;               // m/s
-const int32_t MOTOR_MAX_ROTATION_SPEED = 4;               // m/s
-const uint8_t MOTOR_BRAKE_TIME         = 75;              // ms
+const int32_t MOTOR_BASE_PWM           = 85;    // ‰
+const float   MOTOR_BASE_SPEED         = 0.75F; // m/s
+const int16_t MOTOR_MAX_PWM            = 1000;  // ‰
+const float   MOTOR_MAX_SPEED          = 4;     // m/s
+const int32_t MOTOR_MAX_DECELERATION   = 4;     // m/s
+const int32_t MOTOR_MAX_ROTATION_SPEED = 4;     // m/s
+const uint8_t MOTOR_BRAKE_TIME         = 75;    // ms
+const int32_t MOTOR_MAPPING_PWM        = 85;    // ‰
+
 
 const uint16_t VACUUM_BASE_PWM                    = 150;  // ‰
 const uint16_t VACUUM_MIN_PWM                     = 100;  // ‰
 const uint16_t VACUUM_MAX_PWM                     = 1000; // ‰
 const uint8_t  VACUUM_INTERVAL_BETWEEN_INCREMENTS = 2;    // ms
+const uint16_t VACUUM_MAPPING_PWM                 = 130;  // ‰
+
 
 } // namespace RobotEnv
 
@@ -66,3 +76,5 @@ const uint8_t  VACUUM_INTERVAL_BETWEEN_INCREMENTS = 2;    // ms
             RobotEnv::WHEEL_CIRCUMFERENCE)
 
 #endif /* CONTEXT_ROBOTENV_HPP_ */
+
+// Bach - Cello Suite no. 1 in G major BWV 1007
